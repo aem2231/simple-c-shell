3d3204a8780865792e140360271ed4cabb504423
aem2231 <mariojetullaji@gmail.com>
2026-05-28 19:47:30 +0100

    added builtins: cd and exit

--- src/main.c
+++ src/main.c
@@ -2,15 +2,19 @@
 #include <fcntl.h>	// For file control options used with open()
 #include <signal.h> // For signal handling
 #include <stdbool.h>
-#include <stdio.h>	// For standard input/output functions like printf, stderr
-#include <stdlib.h> // For memory allocation (malloc, free) and exit
-#include <string.h> // For string manipulation functions like strcmp, strtok
+#include <stdio.h>	   // For standard input/output functions like printf, stderr
+#include <stdlib.h>	   // For memory allocation (malloc, free) and exit
+#include <string.h>	   // For string manipulation functions like strcmp, strtok
 #include <sys/types.h> // For data types like pid_t
 #include <sys/wait.h>  // For waitpid and related macros
-#include <unistd.h> // For POSIX operating system API, including fork, exec, pipe, chdir
+#include <unistd.h>	   // For POSIX operating system API, including fork, exec, pipe, chdir
+
+#include "shell_builtins.h"
 
 #define MAX_CMD_LEN 1024
 #define MAX_ARGS 64
+
+const char *builtins[] = { "cd", "exit" };
 
 char **
 parse(char *command)
@@ -19,12 +23,15 @@ parse(char *command)
 	int i = 0;
 
 	char *token = strtok(command, " \t\n");
-	while (token != NULL)
-	{					   // iterate through tokens until there is none left
-		args[i++] = token; // and add the tokens to our args
+	while (token != NULL && i < (MAX_ARGS - 1))
+	{								   // iterate through tokens until there is none left
+		args[i++] = token;			   // and add the tokens to our args
 		token = strtok(NULL, " \t\n"); // update token to store the next token
 	}
 	args[i] = NULL; // NULL terminate  arr
+
+	for (int i = 0; args[i] != NULL; i++)
+		printf("%s\n", args[i]);
 
 	return args;
 }
@@ -60,13 +67,14 @@ execute(char **args)
 int
 main()
 {
-
 	char line_buff[MAX_CMD_LEN];
 	int status = 0;
 
 	while (true)
 	{ // REPL
-		printf("[%d]$ ", status);
+		char cwd[200];
+		getcwd(cwd, 200);
+		printf("%s $ ", cwd);
 		fflush(stdout); // force buffer to flush so prompt appears immediately
 		// wait for input
 
@@ -76,8 +84,14 @@ main()
 		char **parsed_args = parse(line_buff);
 
 		if (parsed_args[0] != NULL)
-			status = execute(parsed_args);
-
+		{
+			if (strcmp(parsed_args[0], "cd") == 0)
+				shell_cd(parsed_args);
+			if (strcmp(parsed_args[0], "exit") == 0)
+				shell_exit(parsed_args);
+			else
+				status = execute(parsed_args);
+		}
 		free(parsed_args);
 	}
 }
